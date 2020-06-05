fc9ab4651c9968eb0f0f72be5ea2969b1b47ffcf
Damian Monogue <demonnic@gmail.com>
2020-06-05 17:20:36 -0400

    Make timergauge a proper child of Geyser.Gauge. add docs for ftext. Generate ldocs.

--- src/resources/timergauge.lua
+++ src/resources/timergauge.lua
@@ -1,24 +1,40 @@
-local TimerGauge = Geyser.Container:new({
+--- TimerGauge
+-- Animated countdown timer, extends Geyser.Gauge
+--@module TimerGauge
+local TimerGauge = {
   name = "TimerGaugeClass",
   active = true,
   showTime = true,
   timerCaption = "",
   updateTime = "10",
   autoHide = true,
-})
+  autoShow = true,
+}
 
 function TimerGauge:setStyleSheet(cssFront, cssBack, cssText)
   cssFront = cssFront or self.cssFront
   cssBack = cssBack or self.cssBack
   cssBack = cssBack or self.cssFront .. "background-color: black;"
   cssText = cssText or self.cssText
-  self.gauge:setStyleSheet(cssFront, cssBack, cssText)
+  if cssFront then
+    self.front:setStyleSheet(cssFront)
+  end
+  if cssBack then
+    self.back:setStyleSheet(cssBack)
+  end
+  if cssText then
+    self.text:setStyleSheet(cssText)
+  end
+  --self.gauge:setStyleSheet(cssFront, cssBack, cssText)
   self.cssFront = cssFront
   self.cssBack = cssBack
   self.cssText = cssText
 end
 
+--- Starts the timergauge. Works whether the timer is stopped or not. Does not start a timer which is already at 0
+-- @tparam[opt] boolean show override the autoShow property. If you pass false it will not automatically pass :show()
 function TimerGauge:start(show)
+  if show == nil then show = self.autoShow end
   self.active = true
   if self.timer then
     killTimer(self.timer)
@@ -30,8 +46,10 @@ function TimerGauge:start(show)
   if show then self:show() end
 end
 
+--- Stops the timergauge. Works whether the timer is started or not.
+-- @tparam[opt] boolean hide override the autoHide property. If you pass true it will ensure :hide() is called when stopped
 function TimerGauge:stop(hide)
-  if not self.active then return end
+  if hide == nil then hide = self.autoHide end
   self.active = false
   if self.timer then
     killTimer(self.timer)
@@ -41,21 +59,27 @@ function TimerGauge:stop(hide)
   if hide then self:hide() end
 end
 
+--- Alias for stop.
+-- @tparam[opt] boolean hide override the autoHide property. If you pass true it will ensure :hide() is called when stopped
 function TimerGauge:pause(hide)
   self:stop(hide)
 end
 
-function TimerGauge:reset(show)
+--- Resets the time on the timergauge to its original value. Does not alter the running state of the timer
+function TimerGauge:reset()
   resetStopWatch(self.stopWatchName)
   adjustStopWatch(self.stopWatchName, self.time * -1)
-  if show then self:show() end
+  self:update()
 end
 
+--- Resets and starts the timergauge.
+-- @tparam[opt] boolean show override the autoShow property. If you pass false it will not automatically pass :show(), if you pass true it will.
 function TimerGauge:restart(show)
   self:reset()
   self:start(show)
 end
 
+--- Get the amount of time remaining on the timer, in seconds
 function TimerGauge:getTime()
   local time = getStopWatchTime(self.stopWatchName)
   if time > 0 then
@@ -67,6 +91,7 @@ function TimerGauge:getTime()
   return math.abs(getStopWatchTime(self.stopWatchName))
 end
 
+--- Execute the timer's hook, if there is one.
 function TimerGauge:executeHook()
   local hook = self.hook
   if not hook then return end
@@ -76,136 +101,113 @@ function TimerGauge:executeHook()
     if not f then
       f,e = loadstring(hook)
     end
-    assert(f, string.format("TimerGauge encountered an error while executing the hook for TimerGauge with name: %s error: %s", self.name, tostring(e)))
+    if not f then
+      debugc(string.format("TimerGauge encountered an error while executing the hook for TimerGauge with name: %s error: %s", self.name, tostring(e)))
+      return
+    end
     hook = f
   end
   hooktype = type(hook)
-  assert(hooktype == "function", string.format("TimerGauge with name: %s was given a hook which is neither a function nor a string which can be made into one. Provided type was %s", self.name, hooktype))
+  if hooktype ~= "function" then
+    debugc(string.format("TimerGauge with name: %s was given a hook which is neither a function nor a string which can be made into one. Provided type was %s", self.name, hooktype))
+    return
+  end
   local worked, err = pcall(hook)
   if not worked then
-    error(string.format("TimerGauge named %s encountered the following error while executing its hook: %s", self.name, err))
+    debugc(string.format("TimerGauge named %s encountered the following error while executing its hook: %s", self.name, err))
   end
 end
 
-function TimerGauge:setToolTip(...)
-  self.gauge:setToolTip(...)
+--- Sets the timer's remaining time to 0, stops it, and executes the hook if one exists.
+-- @tparam[opt] boolean skipHook use true to have it set the timer to 0 and stop, but not execute the hook.
+function TimerGauge:finish(skipHook)
+  resetStopWatch(self.stopWatchName)
+  self:update(skipHook)
 end
 
-function TimerGauge:setColor(...)
-  self.gauge:setColor(...)
+--- Updates the visual representation of the gauge with time remaining
+-- @tparam[opt] boolean skipHook use true if you do not want to execute the hook if the timer is at 0
+function TimerGauge:update(skipHook)
+  local time = self:getTime()
+  local caption = self.timerCaption or ""
+  local text = (self.showTime and string.format("%.1f %s", time, caption)) or caption
+  self:setValue(time, self.time, text)
+  if time == 0 then
+    self:stop(self.autoHide)
+    resetStopWatch(self.stopWatchName)
+    if not skipHook then self:executeHook() end
+  end
 end
 
-function TimerGauge:setFormat(...)
-  self.gauge:setFormat(...)
-end
-
-function TimerGauge:setBold(...)
-  self.gauge:setBold(...)
-end
-
-function TimerGauge:setItalics(...)
-  self.gauge:setItalics(...)
-end
-
-function TimerGauge:setUnderline(...)
-  self.gauge:setUnderline(...)
-end
-
-function TimerGauge:setStrikethrough(...)
-  self.gauge:setStrikethrough(...)
-end
-
-function TimerGauge:setFontSize(...)
-  self.gauge:setFontSize(...)
-end
-
-function TimerGauge:setAlignment(...)
-  self.gauge:setAlignment(...)
-end
-
-function TimerGauge:setFgColor(...)
-  self.gauge:setFgColor(...)
-end
-
-function TimerGauge:enableClickthrough(...)
-  self.gauge:enableClickthrough(...)
-end
-
-function TimerGauge:disableClickthrough(...)
-  self.gauge:disableClickthrough(...)
-end
-
-function TimerGauge:resetToolTip(...)
-  self.gauge:resetToolTip(...)
-end
-
-function TimerGauge:setValue(...)
-  self.gauge:setValue(...)
-end
-
-function TimerGauge:update()
-  if self.active then
-    local time = self:getTime()
-    local caption = self.timerCaption or ""
-    local text = (self.showTime and string.format("%.1f %s", time, caption)) or caption
-    self:setValue(time, self.time, text)
-    if time == 0 then
-      self:stop(self.autoHide)
-      resetStopWatch(self.stopWatchName)
-      self:executeHook()
-    end
+--- Sets the amount of time the timer will run for
+-- @tparam number time how long in seconds the timer should run for
+function TimerGauge:setTime(time)
+  local timetype = type(time)
+  if timetype ~= "number" then
+    local err = string.format("TimerGauge:setTime(time): time as number expected, got %s", timetype)
+    debugc(err)
+    return nil, err
+  end
+  time = math.abs(time)
+  if time == 0 then
+    local err = "TimerGauge:setTime(time): you cannot pass in 0 as the max time for the timer"
+    debugc(err)
+    return nil, err
+  end
+  local currentTime = self:getTime()
+  self.time = time
+  if time < currentTime then
+    self:reset()
   else
-    if self.timer then
-      killTimer(self.timer)
-      self.timer = nil
-    end
+    self:update()
   end
 end
 
 TimerGauge.parent = Geyser.Gauge
+setmetatable(TimerGauge, Geyser.Gauge)
 
 function TimerGauge:new(cons, parent)
   -- type checking and error handling
   local consType = type(cons)
-  assert(consType == "table", string.format("TimerGauge:new(options, parent): options must be provided as a table, received: %s", consType))
+  if consType ~= "table" then
+    local err = string.format("TimerGauge:new(options, parent): options must be provided as a table, received: %s", consType)
+    debugc(err)
+    return nil, err
+  end
+  local timetype = type(cons.time)
   local time = tonumber(cons.time)
-  assert(time, "TimerGauge:new(options, parent): options table must include a time entry, which must be a number")
+  if not time then
+    local err = string.format("TimerGauge:new(options, parent): options table must include a time entry, which must be a number. We received: %s which is type: %s", cons.time or tostring(cons.time), timetype)
+    debugc(err)
+    return nil, err
+  end
   cons.time = math.abs(time)
+  if cons.time == 0 then
+    local err = "TimerGauge:new(options, parent): time entry in options table must be non-0"
+    debugc(err)
+    return nil, err
+  end
 
-  -- make a container
+  -- call parent constructor
   local me = self.parent:new(cons, parent)
   -- add TimerGauge as the metatable/index
   setmetatable(me, self)
   self.__index = self
 
-  -- copy the constraints used for the TimerGauge
-  local gaugeCons = table.deepcopy(cons)
-  -- adjust some properties since the container has the positioning information
-  gaugeCons.x = 0
-  gaugeCons.y = 0
-  gaugeCons.height = "100%"
-  gaugeCons.width = "100%"
-  gaugeCons.name = me.name .. "_timergauge"
-  -- make the gauge
-  me.gauge = Geyser.Gauge:new({
-    x = 0,
-    y = 0,
-    height = "100%",
-    width = "100%",
-    name = me.name .. "_timergauge"
-  }, me)
-  -- and apply any styling requested
+  -- apply any styling requested
   if me.cssFront then
     if not me.cssBack then
       me.cssBack = me.cssFront .. "background-color: black;"
     end
     me:setStyleSheet(me.cssFront, me.cssBack, me.cssText)
   end
+
   -- create and reset the driving stopwatch
   me.stopWatchName = me.name .. "_timergauge"
   createStopWatch(me.stopWatchName)
   me:reset()
-  -- activate?
+
+  -- start it up?
   if me.active then
     me:start()
   end
