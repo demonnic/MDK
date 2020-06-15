a53ea64ffeeccee0883b494ce2f614fc4273d02a
Damian Monogue <demonnic@gmail.com>
2020-06-14 21:02:08 -0400

    Changing package name from MDKExample to just MDK, as it's the full reference package. Getting ready for first nonalpha release

--- src/scripts/MDKExample/Example.lua
+++ src/scripts/MDKExample/Example.lua
@@ -1,10 +1,10 @@
 MDKExample = MDKExample or {}
 function MDKExample.exampleFText()
-  local fText = require("MDKExample-1.ftext")
+  local fText = require("@PKGNAME@.ftext")
   cecho(fText.fText("Testing!", {width = 40, formatType = 'c', textColor = '<orange>', capColor = '<purple>', cap = '[TEST]'}))
 end
 function MDKExample.exampleEMCO()
-  local EMCO = require("MDKExample-1.EMCO")
+  local EMCO = require("@PKGNAME@.EMCO")
   MDKExample.UW = Geyser.UserWindow:new({name = "TestWindow"})
   local stylesheet = [[background-color: rgb(0,180,0,255); border-width: 1px; border-style: solid; border-color: gold; border-radius: 10px;]]
   local istylesheet = [[background-color: rgb(60,60,60,255); border-width: 1px; border-style: solid; border-color: gold; border-radius: 10px;]]
