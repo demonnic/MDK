c0212096e9dffff300168880ff9b7a8945cd7097
Damian Monogue <demonnic@gmail.com>
2020-06-19 20:24:19 -0400

    fixing ldoc.css and making the release prep script copy it out

--- prep-releases.sh
+++ prep-releases.sh
@@ -3,6 +3,11 @@
 basedir=`dirname "$0"`
 srcdir=$basedir/src/resources
 
+# prep the ldoc css file to all sub-projects we are making docs for
+cp $basedir/ldoc.css sub-projects/TextGauges/ldoc.css
+cp $basedir/ldoc.css sub-projects/fText/ldoc.css
+cp $basedir/ldoc.css sub-projects/EMCO/ldoc.css
+
 # prep TextGauges
 cp $srcdir/TextGauges.lua sub-projects/TextGauges/src/resources/TextGauges.lua
 
@@ -10,9 +15,6 @@ cp $srcdir/TextGauges.lua sub-projects/TextGauges/src/resources/TextGauges.lua
 cp $srcdir/ftext.lua sub-projects/fText/src/resources/ftext.lua
 cp $srcdir/textformatter.lua sub-projects/fText/src/resources/textformatter.lua
 cp $srcdir/tablemaker.lua sub-projects/fText/src/resources/tablemaker.lua
-
-# prep luaunit
-cp $srcdir/luaunit.lua sub-projects/luaunit/src/resources/luaunit.lua
 
 # prep EMCO
 cp $srcdir/EMCO.lua sub-projects/EMCO/src/resources/EMCO.lua
