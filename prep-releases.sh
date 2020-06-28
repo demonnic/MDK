de89736c19c76de099954ccc27022db815250e15
Damian Monogue <3660+demonnic@users.noreply.github.com>
2020-06-28 16:17:32 -0400

    Elastic sortbox (#3)
    * fixing ldoc.css and making the release prep script copy it out
    * Moving the :new for Sortbox to the top for doc reasons
    * Add elasticity to SortBox
    * Prepare 1.0.3
    * Add some more automation to prep-releases.sh

--- prep-releases.sh
+++ prep-releases.sh
@@ -2,6 +2,21 @@
 
 basedir=`dirname "$0"`
 srcdir=$basedir/src/resources
+VERSION=$1
+releaseFileName="build/demonnic-MDK-$VERSION.zip"
+if [ -z $VERSION ]; then
+  echo You must provide a version as the first and only argument
+  exit 1
+fi
+
+muddle
+./gendoc.sh
+zip -r -j $releaseFileName src/resources/*
+zip -u $releaseFileName doc/* doc/*/*
+# prep the ldoc css file to all sub-projects we are making docs for
+cp $basedir/ldoc.css sub-projects/TextGauges/ldoc.css
+cp $basedir/ldoc.css sub-projects/fText/ldoc.css
+cp $basedir/ldoc.css sub-projects/EMCO/ldoc.css
 
 # prep TextGauges
 cp $srcdir/TextGauges.lua sub-projects/TextGauges/src/resources/TextGauges.lua
@@ -10,9 +25,6 @@ cp $srcdir/TextGauges.lua sub-projects/TextGauges/src/resources/TextGauges.lua
 cp $srcdir/ftext.lua sub-projects/fText/src/resources/ftext.lua
 cp $srcdir/textformatter.lua sub-projects/fText/src/resources/textformatter.lua
 cp $srcdir/tablemaker.lua sub-projects/fText/src/resources/tablemaker.lua
-
-# prep luaunit
-cp $srcdir/luaunit.lua sub-projects/luaunit/src/resources/luaunit.lua
 
 # prep EMCO
 cp $srcdir/EMCO.lua sub-projects/EMCO/src/resources/EMCO.lua
