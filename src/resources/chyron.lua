b9f5779a1a308f2793939115b30d881de9fe6e65
Damian Monogue <demonnic@gmail.com>
2020-07-07 14:17:22 -0400

    New release, add Chyron

---
+++ src/resources/chyron.lua
@@ -0,0 +1,216 @@
+--- Creates a label with a scrolling text element. It is highly recommended you use a monospace font for this label.
+--@classmod Chyron
+local Chyron = {
+  name = "ChyronClass",
+  text = "",
+	displayWidth = 28,
+  updateTime = 200,
+  font = "Bitstream Vera Sans Mono",
+  fontSize = "9",
+  autoWidth = true,
+  delimiter = "|",
+	pos = 1,
+  enabled = true,
+  alignment = "center",
+}
+
+--- Creates a new Chyron label
+-- @tparam table cons table of constraints which configures the EMCO.
+-- <table class="tg">
+-- <thead>
+--   <tr>
+--     <th>option name</th>
+--     <th>description</th>
+--     <th>default</th>
+--   </tr>
+-- </thead>
+-- <tbody>
+--   <tr>
+--     <td class="tg-odd">text</td>
+--     <td class="tg-odd">The text to scroll on the label</td>
+--     <td class="tg-odd">""</td>
+--   </tr>
+--   <tr>
+--     <td class="tg-even">updateTime</td>
+--     <td class="tg-even">Milliseconds between movements (one letter shift)</td>
+--     <td class="tg-even">200</td>
+--   </tr>
+--   <tr>
+--     <td class="tg-odd">displayWidth</td>
+--     <td class="tg-odd">How many chars wide to display the text</td>
+--     <td class="tg-odd">28</td>
+--   </tr>
+--   <tr>
+--     <td class="tg-even">delimiter</td>
+--     <td class="tg-even">This character will be inserted with a space either side to mark the stop/start of the message</td>
+--     <td class="tg-even">"|"</td>
+--   </tr>
+--   <tr>
+--     <td class="tg-odd">enabled</td>
+--     <td class="tg-odd">Should the chyron scroll?</td>
+--     <td class="tg-odd">true</td>
+--   </tr>
+--   <tr>
+--     <td class="tg-even">font</td>
+--     <td class="tg-even">What font to use for the Chyron? Available in Geyser.Label but we define a default.</td>
+--     <td class="tg-even">"Bitstream Vera Sans Mono"</td>
+--   </tr>
+--   <tr>
+--     <td class="tg-odd">fontSize</td>
+--     <td class="tg-odd">What font size to use for the Chyron? Available in Geyser.Label but we define a default.</td>
+--     <td class="tg-odd">9</td>
+--   </tr>
+--   <tr>
+--     <td class="tg-even">autoWidth</td>
+--     <td class="tg-even">Should the Chyron resize to just fit the text?</td>
+--     <td class="tg-even">true</td>
+--   </tr>
+--   <tr>
+--     <td class="tg-odd">alignment</td>
+--     <td class="tg-odd">What alignment(left/right/center) to use for the Chyron text? Available in Geyser.Label but we define a default.</td>
+--     <td class="tg-odd">"center"</td>
+--   </tr>
+-- </tbody>
+-- </table>
+-- @tparam GeyserObject container The container to use as the parent for the Chyron
+function Chyron:new(cons, container)
+  cons = cons or {}
+  cons.type = cons.type or "Chyron"
+	local me = self.parent:new(cons, container)
+	setmetatable(me, self)
+	self.__index = self
+  me.pos = 0
+  me:setDisplayWidth(self.displayWidth)
+  me:setMessage(me.text)
+  if me.enabled then
+    me:start()
+  else
+    me:stop()
+  end
+	return me
+end
+
+--- Sets the numver of characters of the text to display at once
+--@tparam number displayWidth number of characters to show at once
+function Chyron:setDisplayWidth(displayWidth)
+  displayWidth = displayWidth or self.displayWidth
+  self.displayWidth = displayWidth
+  if self.autoWidth then
+    local width = calcFontSize(self.fontSize, self.font)
+    self:resize(width * (displayWidth + 2), self.height)
+  end
+	if not self.enabled then
+	  self.pos = self.pos -1
+		self:doScroll()
+	end
+end
+
+--- Override setFontSize to call setDisplayWidth in order to resize if necessary
+--@local
+function Chyron:setFontSize(fontSize)
+  Geyser.Label.setFontSize(self, fontSize)
+  self:setDisplayWidth()
+end
+
+--- Override setFont to call setDisplayWidth in order to resize if necessary
+--@local
+function Chyron:setFont(font)
+  Geyser.Label.setFont(self, font)
+  self:setDisplayWidth()
+end
+
+--- Returns the proper section of text
+--@local
+--@param text the text to extract a section of
+--@param start the character to start at
+--@param the length of the text you want to extract
+function Chyron:scrollText(start, length)
+  local t = self.textTable
+  local s = ''
+  local e = start+length
+  for i=start-1, e-2 do
+    local n = (i % #t) + 1
+    s = s..t[n]
+  end
+  return s
+end
+
+--- scroll the text
+--@local
+function Chyron:doScroll()
+  self.pos = self.pos + 1
+	local displayString = self:scrollText(self.pos, self.displayWidth)
+	self:echo('&lt;'.. displayString ..'&gt;')
+	self.message = self.text
+end
+
+--- Sets the Chyron from the first position, without changing enabled status
+function Chyron:reset()
+  self.pos = 0
+	if not self.enabled then self:doScroll() end
+end
+
+--- Stops the Chyron with its current display
+function Chyron:pause()
+  self.enabled = false
+  if self.timer then killTimer(self.timer) end
+end
+
+--- Start the Chyron back up from wherever it currently is
+function Chyron:start()
+  self.enabled = true
+  if self.timer then killTimer(self.timer) end
+  self.timer = tempTimer(self.updateTime / 1000, function() self:doScroll() end, true)
+end
+
+--- Change the update time for the Chyron
+--@tparam number updateTime new updateTime in milliseconds
+function Chyron:setUpdateTime(updateTime)
+  self.updateTime = updateTime or self.updateTime
+  if self.timer then killTimer(self.timer) end
+  if self.enabled then self:start() end
+end
+
+--- Enable autoWidth adjustment
+function Chyron:enableAutoWidth()
+  self.autoWidth = true
+  self:setDisplayWidth()
+end
+
+--- Disable autoWidth adjustment
+function Chryron:disableAutoWidth()
+  self.autoWidth = false
+end
+
+--- Stop the Chyron, and reset it to the original position
+function Chyron:stop()
+  if self.timer then killTimer(self.timer) end
+  self.enabled = false
+  self.pos = 0
+	self:doScroll()
+end
+
+--- Change the text being scrolled on the Chyron
+--@tparam string message the text you want to have scroll on the Chyron
+function Chyron:setMessage(message)
+  self.text = message
+  self.pos = 0
+  message = string.format("%s %s ", message, self.delimiter)
+  local t = {}
+  for i=1, #message do
+    t[i] = message:sub(i, i)
+  end
+  self.textTable = t
+	if not self.enabled then self:doScroll() end
+end
+
+--- Change the delimiter used to show the beginning and end of the message
+--@tparam string delimiter the new delimiter to use. I recommend using one character.
+function Chyron:setDelimiter(delimiter)
+  self.delimiter = delimiter
+end
+
+Chyron.parent = Geyser.Label
+setmetatable(Chyron, Geyser.Label)
+
+return Chyron
