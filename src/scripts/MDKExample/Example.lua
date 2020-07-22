b26a1432e9e6c26ce34259a791ddcc8347c4ee6b
Damian Monogue <demonnic@gmail.com>
2020-07-22 18:10:48 -0400

    Version bump

--- src/scripts/MDKExample/Example.lua
+++ src/scripts/MDKExample/Example.lua
@@ -1,4 +1,4 @@
-MDKExample = MDKExample or { version = "1.0.7" }
+MDKExample = MDKExample or { version = "1.0.8" }
 function MDKExample.exampleFText()
   local fText = require("@PKGNAME@.ftext")
   cecho(fText.fText("Testing!", {width = 40, formatType = 'c', textColor = '<orange>', capColor = '<purple>', cap = '[TEST]'}))
