f90462dd6bdf553e0553d3aa4291e108631816cb
Damian Monogue <demonnic@gmail.com>
2020-10-19 19:56:21 -0400

    Fixing up some of Archon and maybe looking at finishing it sometime this year

--- src/resources/archon.lua
+++ src/resources/archon.lua
@@ -1,4 +1,5 @@
 local Archon = {
+  name = "ArchonClass",
   formatType = "d",
   headCharacter = "*",
   footCharacter = "*",
@@ -7,7 +8,8 @@ local Archon = {
   rowSeparator = "-",
   separator = "|",
   separatorColor = "white",
-  configOptions = {},
+  configDescription = {},
+  location = "",
 }
 
 local pathOfThisFile = (...):match("(.-)[^%.]+$")
@@ -26,103 +28,59 @@ local sfo = s.FormatOutput
 local stbl = s.Table
 local tsize = table.size
 local stest = s.Test
-local valid_types =oneOf("string", "list", "int", "float", "map", "boolean")
+local valid_types = oneOf("string", "list", "int", "float", "map", "boolean")
 local no_index_gaps = function(obj) return #obj == tsize(obj) end
 local snogap = stest(no_index_gaps, "Table has noninteger keys or a gap in its indices, must be parseable using ipairs")
 local slist = allOf(stbl, snogap)
 
-local type_int_schema = srec {
-  type = "int",
-  default = sint,
-  min = sint,
-  max = sint,
-  step = sint,
-  displayName = sstring
+local type_schema = {
+  int = srec {
+    type = "int",
+    default = sint,
+    min = sint,
+    max = sint,
+    step = sint,
+    displayName = sstring
+  },
+  float = srec {
+    type = "float",
+    default = snum,
+    min = snum,
+    max = snum,
+    step = snum,
+    displayName = sstring
+  },
+  string = srec {
+    type = "string",
+    default = oneOf(snil,sstring),
+    allowedValues = oneOf(snil, allOf(scol(sstring), slist)),
+    displayName = sstring
+  },
+  map = srec {
+    type = "map",
+    default = stbl,
+    displayName = sstring
+  },
+  list = srec {
+    type = "list",
+    default = oneOf(snil, slist),
+    allowedTypes = oneOf(snil, scol(valid_types), valid_types),
+    minSize = oneOf(snil, sint),
+    displayName = sstring
+  },
+  boolean = srec {
+    type = "boolean",
+    default = oneOf(snil, sbool),
+    displayName = sstring
+  },
 }
-
-local type_float_schema = srec {
-  type = "float",
-  default = snum,
-  min = snum,
-  max = snum,
-  step = snum,
-  displayName = sstring
-}
-
-local type_string_schema = srec {
-  type = "string",
-  default = oneOf(snil,sstring),
-  allowedValues = oneOf(snil, allOf(scol(sstring), slist)),
-  displayName = sstring
-}
-
-local type_map_schema = srec {
-  type = "map",
-  default = stbl,
-  displayName = sstring
-}
-
-local type_list_schema = srec {
-  type = "list",
-  default = oneOf(snil, slist),
-  allowedTypes = oneOf(snil, scol(valid_types), valid_types),
-  minSize = oneOf(snil, sint),
-  displayName = sstring
-}
-
-local type_boolean_schema = srec {
-  type = "boolean",
-  default = oneOf(snil, sbool),
-  displayName = sstring
-}
-
-local function verify_string_entry(entry)
-  local err = scs(entry, type_string_schema)
-  if err then return sfo(err) end
-end
-
-local function verify_map_entry(entry)
-  local err = scs(entry, type_map_schema)
-  if err then return sfo(err) end
-end
-
-local function verify_list_entry(entry)
-  local err = scs(entry, type_list_schema)
-  if err then return sfo(err) end
-end
-
-local function verify_int_entry(entry)
-  local err = scs(entry, type_int_schema)
-  if err then return sfo(err) end
-end
-
-local function verify_float_entry(entry)
-  local err = scs(entry, type_float_schema)
-  if err then return sfo(err) end
-end
-
-local function verify_boolean_entry(entry)
-  local err = scs(entry, type_boolean_schema)
-  if err then return sfo(err) end
-end
-
 local function verify_entry(entry)
   local entry_type = entry.type
-  if entry_type == "string" then
-    return verify_string_entry(entry)
-  elseif entry_type == "map" then
-    return verify_map_entry(entry)
-  elseif entry_type == "list" then
-    return verify_list_entry(entry)
-  elseif entry_type == "int" then
-    return verify_int_entry(entry)
-  elseif entry_type == "float" then
-    return verify_float_entry(entry)
-  elseif entry_type == "boolean" then
-    return verify_boolean_entry(entry)
-  else
-    return "Entry is not a known type, please select from 'string', 'map', 'list', 'int', and 'float'"
+  if scs(entry_type, valid_types) then
+    return "Entry is not a known type, please select from 'string', 'map', 'list', 'int', and 'float'. You provided: " .. entry_type
   end
+  local err = scs(entry, type_schema[entry_type])
+  if err then return sfo(err) end
 end
 
 function Archon.verify_entry(entry)
@@ -131,6 +89,48 @@ function Archon.verify_entry(entry)
   err = scs(entry.type, valid_types)
   if err then return err end
   return verify_entry(entry)
+end
+
+local function validate_config(config)
+  local errors = {}
+  for _,descriptor in pairs(config) do
+    local err = Archon.verify_entry(descriptor)
+    if err ~= nil then
+      errors[#errors+1] = err
+    end
+  end
+  if not table.is_empty(errors) then
+    return table.concat(errors, "\n")
+  end
+  return nil
+end
+
+function Archon:checkConfig()
+  return validate_config(self.config)
+end
+
+function Archon:getConfig()
+  return Archon.getValueAt(self.location)
+end
+
+function Archon:load()
+  local savedConfig = {}
+  table.load(self.saveFile, savedConfig)
+  local config = self:getConfig()
+  if not config then 
+    self:createDefaultConfig()
+    config = self:getConfig()
+  end
+  table.update(config, savedConfig)
+  Archon:verify()
+end
+
+function Archon:save()
+  local config = self:getConfig()
+  table.save(self.saveFile, config)
+end
+
+function Archon:verify()
 end
 
 local function digForValue(dataFrom, tableTo)
