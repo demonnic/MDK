014da0a4a73144ba209f35d1951fb247a7eb3b21
Damian <demonnic@gmail.com>
2022-06-07 12:59:15 -0400

    More tests, this time for TableMaker

--- src/resources/test/ftext_spec.lua
+++ src/resources/test/ftext_spec.lua
@@ -27,28 +27,21 @@ describe("ftext:", function()
 
     it("Should wrap lines to the correct length", function()
       local str = "This is a test of the emergency broadcast system. This is only a test"
-      local options = {
-        width = 10,
-        alignment = "centered",
-      }
+      local options = {width = 10, alignment = "centered"}
       local actual = fText(str, options)
-      for _,line in ipairs(actual:split("\n")) do
+      for _, line in ipairs(actual:split("\n")) do
         assert.equals(line:len(), 10)
       end
       options.width = 15
       actual = fText(str, options)
-      for _,line in ipairs(actual:split("\n")) do
+      for _, line in ipairs(actual:split("\n")) do
         assert.equals(line:len(), 15)
       end
     end)
 
     describe("non-space spacer character:", function()
       local str = "some text"
-      local options = {
-        width = "20",
-        alignment = "left",
-        spacer = "="
-      }
+      local options = {width = "20", alignment = "left", spacer = "="}
       it("Should work with left align", function()
         local expected = "some text =========="
         local actual = fText(str, options)
@@ -75,12 +68,7 @@ describe("ftext:", function()
 
     describe("nogap option:", function()
       local str = "some text"
-      local options = {
-        width = "20",
-        alignment = "left",
-        spacer = "=",
-        nogap = true,
-      }
+      local options = {width = "20", alignment = "left", spacer = "=", nogap = true}
 
       it("Should work with left align", function()
         local expected = "some text==========="
@@ -108,11 +96,7 @@ describe("ftext:", function()
 
     describe("cap functionality", function()
       local str = "some text"
-      local options = {
-        width = 20,
-        spacer = "=",
-        cap = "|",
-      }
+      local options = {width = 20, spacer = "=", cap = "|"}
 
       it("Should place the spacer outside the cap by default", function()
         local expected = "===| some text |===="
@@ -172,7 +156,7 @@ describe("ftext:", function()
       mirror = true,
       capColor = "<purple>",
       spacerColor = "<green>",
-      textColor = "<red>"
+      textColor = "<red>",
     }
     it("Should handle cecho colored text", function()
       local expectedStripped = "[=== some text ====]"
@@ -194,17 +178,14 @@ describe("ftext:", function()
 
     it("Should wrap cecho lines to the correct length", function()
       local str = "This is a test of the emergency broadcast system. This is only a test"
-      local options = {
-        width = 10,
-        alignment = "centered",
-      }
+      local options = {width = 10, alignment = "centered"}
       local actual = cfText(str, options)
-      for _,line in ipairs(actual:split("\n")) do
+      for _, line in ipairs(actual:split("\n")) do
         assert.equals(cecho2string(line):len(), 10)
       end
       options.width = 15
       actual = cfText(str, options)
-      for _,line in ipairs(actual:split("\n")) do
+      for _, line in ipairs(actual:split("\n")) do
         assert.equals(cecho2string(line):len(), 15)
       end
     end)
@@ -244,17 +225,14 @@ describe("ftext:", function()
 
     it("Should wrap decho lines to the correct length", function()
       local str = "This is a test of the emergency broadcast system. This is only a test"
-      local options = {
-        width = 10,
-        alignment = "centered",
-      }
+      local options = {width = 10, alignment = "centered"}
       local actual = dfText(str, options)
-      for _,line in ipairs(actual:split("\n")) do
+      for _, line in ipairs(actual:split("\n")) do
         assert.equals(decho2string(line):len(), 10)
       end
       options.width = 15
       actual = dfText(str, options)
-      for _,line in ipairs(actual:split("\n")) do
+      for _, line in ipairs(actual:split("\n")) do
         assert.equals(decho2string(line):len(), 15)
       end
     end)
@@ -293,17 +271,14 @@ describe("ftext:", function()
 
     it("Should wrap hecho lines to the correct length", function()
       local str = "This is a test of the emergency broadcast system. This is only a test"
-      local options = {
-        width = 10,
-        alignment = "centered",
-      }
+      local options = {width = 10, alignment = "centered"}
       local actual = hfText(str, options)
-      for _,line in ipairs(actual:split("\n")) do
+      for _, line in ipairs(actual:split("\n")) do
         assert.equals(hecho2string(line):len(), 10)
       end
       options.width = 15
       actual = hfText(str, options)
-      for _,line in ipairs(actual:split("\n")) do
+      for _, line in ipairs(actual:split("\n")) do
         assert.equals(hecho2string(line):len(), 15)
       end
     end)
@@ -320,7 +295,8 @@ describe("ftext:", function()
 
     it("Should let you change width using :setWidth", function()
       formatter:setWidth(80)
-      local expected = "<white><reset><white>                                  <reset><white> some text <reset><white>                                   <reset><white><reset>"
+      local expected =
+        "<white><reset><white>                                  <reset><white> some text <reset><white>                                   <reset><white><reset>"
       local actual = formatter:format(str)
       assert.equals(expected, actual)
       assert.equals(80, cecho2string(actual):len())
@@ -328,7 +304,7 @@ describe("ftext:", function()
 
     it("Should format for cecho by default", function()
       local expected = "<white><reset><white>    <reset><white> some text <reset><white>     <reset><white><reset>"
-      local expectedStripped ="     some text      "
+      local expectedStripped = "     some text      "
       local actual = formatter:format(str)
       local actualStripped = cecho2string(actual)
       assert.equals(expected, actual)
@@ -357,9 +333,10 @@ describe("ftext:", function()
       assert.equals(expected, actual)
     end)
 
-    it ("Should default to word wrapping, and let you change it with :setWrap", function()
+    it("Should default to word wrapping, and let you change it with :setWrap", function()
       formatter:setWidth(10)
-      local expected = "<white><reset><white>  <reset><white> some <reset><white>  <reset><white><reset>\n<white><reset><white>  <reset><white> text <reset><white>  <reset><white><reset>"
+      local expected =
+        "<white><reset><white>  <reset><white> some <reset><white>  <reset><white><reset>\n<white><reset><white>  <reset><white> text <reset><white>  <reset><white><reset>"
       local actual = formatter:format(str)
       assert.equals(expected, actual)
       expected = "<white><reset><white><reset><white> some text <reset><white><reset><white><reset>"
@@ -384,7 +361,6 @@ describe("ftext:", function()
 
     it("Should allow you to change the spacer color using :setSpacerColor", function()
       formatter:setSpacerColor("<red>")
-      --local expected = "<white><reset><white>    <reset><white> some text <reset><white>     <reset><white><reset>"
       local expected = "<white><reset><red>    <reset><white> some text <reset><red>     <reset><white><reset>"
       local actual = formatter:format(str)
       assert.equals(expected, actual)
@@ -392,7 +368,6 @@ describe("ftext:", function()
 
     it("Should allow you to change the text color using :setTextColor", function()
       formatter:setTextColor("<red>")
-      --local expected = "<white><reset><white>    <reset><white> some text <reset><white>     <reset><white><reset>"
       local expected = "<white><reset><white>    <reset><red> some text <reset><white>     <reset><white><reset>"
       local actual = formatter:format(str)
       assert.equals(expected, actual)
@@ -400,7 +375,7 @@ describe("ftext:", function()
 
     it("Should allow you to change the spacer using :setSpacer", function()
       formatter:setSpacer("=")
-      --local expected = "<white><reset><white>    <reset><white> some text <reset><white>     <reset><white><reset>"
+      -- local expected = "<white><reset><white>    <reset><white> some text <reset><white>     <reset><white><reset>"
       local expected = "<white><reset><white>====<reset><white> some text <reset><white>=====<reset><white><reset>"
       local actual = formatter:format(str)
       assert.equals(expected, actual)
@@ -430,6 +405,43 @@ describe("ftext:", function()
       local expected = "<white><<reset><white>   <reset><white> some text <reset><white>    <reset><white>><reset>"
       local actual = formatter:format(str)
       assert.equal(expected, actual)
+    end)
+  end)
+
+  describe("ftext.TableMaker", function()
+    local TableMaker = ftext.TableMaker
+    local tm
+    before_each(function()
+      tm = TableMaker:new()
+      tm:addColumn({name = "col1", width = 15, textColor = "<red>"})
+      tm:addColumn({name = "col2", width = 15, textColor = "<blue>"})
+      tm:addColumn({name = "col3", width = 15, textColor = "<green>"})
+      tm:addRow({"some text", "more text", "other text"})
+      tm:addRow({"little text", "bigger text", "text"})
+    end)
+
+    it("Should assemble a formatted table given default options", function()
+      local expected = [[<white>*************************************************<reset>
+<white>*<reset><white><reset><white>    <reset><red> col1 <reset><white>     <reset><white><reset><white>|<reset><white><reset><white>    <reset><blue> col2 <reset><white>     <reset><white><reset><white>|<reset><white><reset><white>    <reset><green> col3 <reset><white>     <reset><white><reset><white>*<reset>
+<white>*<reset><white>---------------<reset><white>|<reset><white>---------------<reset><white>|<reset><white>---------------<reset><white>*<reset>
+<white>*<reset><white><reset><white>  <reset><red> some text <reset><white>  <reset><white><reset><white>|<reset><white><reset><white>  <reset><blue> more text <reset><white>  <reset><white><reset><white>|<reset><white><reset><white> <reset><green> other text <reset><white>  <reset><white><reset><white>*<reset>
+<white>*<reset><white>---------------<reset><white>|<reset><white>---------------<reset><white>|<reset><white>---------------<reset><white>*<reset>
+<white>*<reset><white><reset><white> <reset><red> little text <reset><white> <reset><white><reset><white>|<reset><white><reset><white> <reset><blue> bigger text <reset><white> <reset><white><reset><white>|<reset><white><reset><white>    <reset><green> text <reset><white>     <reset><white><reset><white>*<reset>
+<white>*************************************************<reset>
+]]
+      local actual = tm:assemble()
+      assert.equals(expected, actual)
+    end)
+
+    it("TableMaker:getCell should return the text and formatter for a specific cell", function()
+      local expectedText = "more text"
+      local expectedFormatter = tm.columns[2]
+      local actualText, actualFormatter = tm:getCell(1, 2)
+      assert.equals(expectedText, actualText)
+      assert.equals(expectedFormatter, actualFormatter)
+      local expectedFormatted = "<white><reset><white>  <reset><blue> more text <reset><white>  <reset><white><reset>"
+      local actualFormatted = actualFormatter:format(actualText)
+      assert.equals(expectedFormatted, actualFormatted)
     end)
   end)
 end)
